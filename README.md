472158cea6d7e4d95f54302b936d57fa852878eb
Aakash Adesara <aakash.adesara@gmail.com>
2014-02-15 21:25:19 -0800

    cool stuff

---
+++ README.md
@@ -0,0 +1,20 @@
+Space Invaders <br> 
+==============
+
+Creator: Aakash Adesara <br> 
+Where: PennApps 2014 Valentines Day <br> 
+When: February 14th, 2014 - February 16th 2014 <br> 
+Technologies: LeapMotion SDK, JavaScript, HTML, CSS <br>
+
+Credits: <br> 
+<h4> Original Code: </h4>
+<h6> <a href="https://github.com/dwmkerr/spaceinvaders"> https://github.com/dwmkerr </a> </h6>
+
+<h4> Song Credits: </h4>
+<h6> <a href="http://www.youtube.com/watch?v=WlEXKiY2bho"> Unicorn Kid 8Bytes </a> </h6>
+
+<h4> Motivation Credits: </h4>
+<h6> <a href="http://www.pennapps.com"> PennApps </a> and <a href="http://www.leapmotion.com"> Leapmotion </a> </h6>
+
+<h4> Image Credits: </h4>
+<h6> Sprite Sheet: <a href = "http://fontvir.us/x/invaders.gif"> Fontvir </a> Peanut Butter Jelly Time: <a href = "http://knowyourmeme.com/memes/peanut-butter-jelly-time"> GIF </a> </h6>
---
+++ js/starfield.js
@@ -0,0 +1,111 @@
+/*
+	Starfield lets you take a div and turn it into a starfield.
+
+*/
+
+//	Define the starfield class.
+function Starfield() {
+	this.fps = 30;
+	this.canvas = null;
+	this.width = 0;
+	this.width = 0;
+	this.minVelocity = 200;
+	this.maxVelocity = 500;
+	this.stars = 1000;
+	this.intervalId = 0;
+}
+
+//	The main function - initialises the starfield.
+Starfield.prototype.initialise = function(div) {
+	var self = this;
+
+	//	Store the div.
+	this.containerDiv = div;
+	self.width = window.innerWidth;
+	self.height = window.innerHeight;
+
+	window.onresize = function(event) {
+		self.width = window.innerWidth;
+		self.height = window.innerHeight;
+		self.canvas.width = self.width;
+		self.canvas.height = self.height;
+		self.draw();
+ 	}
+
+	//	Create the canvas.
+	var canvas = document.createElement('canvas');
+	div.appendChild(canvas);
+	this.canvas = canvas;
+	this.canvas.width = this.width;
+	this.canvas.height = this.height;
+};
+
+Starfield.prototype.start = function() {
+
+	//	Create the stars.
+	var stars = [];
+	for(var i=0; i<this.stars; i++) {
+		stars[i] = new Star(Math.random()*this.width, Math.random()*this.height, Math.random()*3+1,
+		 (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+	}
+	this.stars = stars;
+
+	var self = this;
+	//	Start the timer.
+	this.intervalId = setInterval(function() {
+		self.update();
+		self.draw();	
+	}, 1000 / this.fps);
+};
+
+Starfield.prototype.stop = function() {
+	clearInterval(this.intervalId);
+};
+
+Starfield.prototype.update = function() {
+	var dt = 1 / this.fps;
+
+	for(var i=0; i<this.stars.length; i++) {
+		var star = this.stars[i];
+		star.y += dt * star.velocity;
+		//	If the star has moved from the bottom of the screen, spawn it at the top.
+		if(star.y > this.height) {
+			this.stars[i] = new Star(Math.random()*this.width, 0, Math.random()*3+1, 
+		 	(Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+		}
+	}
+};
+
+Starfield.prototype.draw = function() {
+
+	
+	//	Get the drawing context.
+	var ctx = this.canvas.getContext("2d");
+
+	//	Draw the background.
+ 	ctx.fillStyle = '#000000';
+	ctx.fillRect(0, 0, this.width, this.height);
+
+	//	Draw stars.
+	ctx.fillStyle = 'yellow';
+
+	var array = [ 'yellow', 'red', 'purple', 'blue', 'green', 'orange'];
+
+	var lengthsArray = [20, 40, 60, 80, 100, 120, 140, 160];
+
+	var widthsArray = [0.5, 1.0, 1.5, 2.0];
+
+	for(var i=0; i<this.stars.length;i++) {
+		var star = this.stars[i];
+		ctx.fillStyle = array[Math.floor(Math.random() * array.length)];
+		ctx.fillRect(star.x, star.y, widthsArray[Math.floor(Math.random() * widthsArray.length)], 
+			lengthsArray[Math.floor(Math.random() * lengthsArray.length)]);
+	}
+};
+
+function Star(x, y, size, velocity) {
+	this.x = x;
+	this.y = y; 
+	this.size = size;
+	this.velocity = velocity;
+}
